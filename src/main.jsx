4c84ef3d0453fe1237b3bee615261a8acb338150
Fernando <fernandostarlin1012@gmail.com>
2023-04-26 16:17:57 -0400

    Creating the root layout component

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
 import "./index.css";
+import Root from "./routes/root.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />;
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
