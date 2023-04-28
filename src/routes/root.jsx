86f445a621b603f23c452d5f1ace5bd7cf89b712
Fernando <fernandostarlin1012@gmail.com>
2023-04-28 14:27:34 -0400

    Get submissions with client side routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -15,8 +15,10 @@ export async function action() {
   return redirect(`/contacts/${contacts.id}/edit`);
 }
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
@@ -28,7 +30,7 @@ export default function Root() {
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               type="search"
               id="q"
@@ -39,7 +41,7 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true}>
               <div className="sr-only" aria-live="polite"></div>
             </div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
